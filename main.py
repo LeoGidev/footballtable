0df6f02160fddfd2eca978f87285d67892d64c54
Leo Gimenez <leogimenez.dev@gmail.com>
2024-10-23 14:57:56 -0300

    configuraciones de buttons

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ class FootballApp:
         self.root = root
         self.root.title("Tabla de posiciones y goleadores")
         self.root.geometry("400x200")
+        self.upload_button = tk.Button(self.root, text="Cargar archivo Excel", command=self.upload_file)
+        self.upload_button.pack(pady=20)
